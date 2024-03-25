005b2405c721d9b1f706166ea7c4cedf842cbbe8
Johan Alejandro Peña <johanpena998@gmail.com>
2024-03-25 08:47:18 -0300

    change name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://Johan-Alejandro.github.io/adopta-perros",
+  base: "https://johan-alejandro.github.io/adopta-perros",
 });
